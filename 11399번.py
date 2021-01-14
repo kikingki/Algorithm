792ff4fd6af2cac97c4156da35ade3c6603e0778
kikingki <mongbu54@gmail.com>
2021-01-14 18:33:14 +0900

    Update BOJ 11399

--- 11399번.py
+++ 11399번.py
@@ -2,14 +2,15 @@ import sys
 input = sys.stdin.readline
 
 p = int(input())
-t = list(map(int, input().split()))
-time = 0
-sum = 0
+# t = list(map(int, input().split()))
+order = sorted(map(int, input().split()))
+t = 0
+ac = 0
 
-t.sort()    # 인출시간이 빠른 순서대로 정렬
+# t.sort()    # 인출시간이 빠른 순서대로 정렬
 
-for i in range(0,len(t)):
-    sum += t[i]     #각 사람이 인출하는 데 걸리는 시간
-    time += sum      #인출시간의 총합
+for i in range(0,p):
+    ac += order[i]     #각 사람이 인출하는 데 걸리는 시간
+    t += ac      #인출시간의 총합
 
-print(time)
+print(t)
