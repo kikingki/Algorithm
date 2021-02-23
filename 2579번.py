8ab0570fd300e0162a5a64ba727da20d5ede5733
kikingki <mongbu54@gmail.com>
2021-02-23 18:42:20 +0900

    BOJ 2579

--- 2579번.py
+++ 2579번.py
@@ -23,6 +23,6 @@ dp[1] = stair[0]+stair[1]
 dp[2] = max(stair[0]+stair[2], stair[1]+stair[2])
 
 for i in range(3, n):
-    dp[i] = max(dp[i-2] + stair[i], dp[i-3] + stair[i-1] + stair[i])
+    dp[i] = max(dp[i-2], dp[i-3] + stair[i-1]) + stair[i]
 
 print(dp[n-1])
